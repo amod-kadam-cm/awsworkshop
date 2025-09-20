7b47496544c7708a2a03a620b86fa7181e490cef
Amod <amod@Amods-MacBook-Pro.local>
2025-09-20 20:54:29 +0530

    Fixed the JIRA-4

--- test/script.sh
+++ test/script.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
+file="/var/log/myapp/output.txt"
 
-echo "Hello World!" >> hello.txt 
-
-file="output.txt"
-
+mkdir -p /var/log/myapp
 echo "Script started at $(date)" >> "$file"
 echo "Writing multiple lines..." >> "$file"
 echo "Done." >> "$file"
